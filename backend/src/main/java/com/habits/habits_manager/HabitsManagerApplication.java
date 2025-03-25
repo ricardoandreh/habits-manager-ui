cc0f372caeeb3d6a9b7103b268ea3dd156704d58
ricardoandreh <ricardo.andre.ifc@gmail.com>
2025-03-24 22:30:15 -0300

    chore: setup project structure

---
+++ backend/src/main/java/com/habits/habits_manager/HabitsManagerApplication.java
@@ -0,0 +1,13 @@
+package com.habits.habits_manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HabitsManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HabitsManagerApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/habits/habits_manager/HabitsManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.habits.habits_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HabitsManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
